d811c58d6c40f7f2e4dba4d08a9301d647852f37
Jesus Martin <jesusmartinromero10@gmail.com>
2022-07-19 23:38:21 +0200

    romanos diccionarios2

--- first_part.py
+++ first_part.py
@@ -41,7 +41,7 @@ componentes = {
         70 : 'LXX', 80 : 'LXXX', 90 : 'XC',
     
         1 : 'I', 2: 'II', 3 : 'III',
-        4 : 'IL', 5 : 'V', 6 : 'VI',
+        4 : 'IV', 5 : 'V', 6 : 'VI',
         7 : 'VII', 8 : 'VIII', 9 : 'IX'
     
 
