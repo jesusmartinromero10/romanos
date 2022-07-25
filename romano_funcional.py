3597dda6ce61d474b27a58f552a657a99cf9f3ef
Jesus Martin <jesusmartinromero10@gmail.com>
2022-07-25 19:52:35 +0200

    terminado a mi forma

--- romano_funcional.py
+++ romano_funcional.py
@@ -43,6 +43,7 @@ def romano_a_entero2(romano: str) -> int:
     r = 0
     caracter_anterior = ""
     cont_repes = 1
+    compro = ""
     for caracter in romano:
         
         if caracter == caracter_anterior:
@@ -50,15 +51,33 @@ def romano_a_entero2(romano: str) -> int:
         else:
             cont_repes = 1
 
+        if (caracter == "D" and caracter_anterior == "D") or (caracter == "L" and caracter_anterior == "L") or (caracter == "V" and caracter_anterior == "V"):
+            raise RomanNumberError("No se puede repetir D o L o V mas de una vez")
+
         if cont_repes > 3:
             raise RomanNumberError("No se pueden dar mas de tres repeticiones")
 
         if simbolo_romano[caracter] > simbolo_romano[caracter_anterior]:
-            r -= simbolo_romano[caracter_anterior] * 2
+            if caracter_anterior == "I" and (caracter != "X" and caracter != "V"):
+                raise RomanNumberError("No se puede restar I a un valor distinto de X o V")
+            if caracter_anterior == "X" and (caracter != "L" and caracter != "C"):
+                raise RomanNumberError("No se puede restar X a un valor distinto de L o C")
+            if caracter_anterior == "C" and (caracter != "D" and caracter != "M"):
+                raise RomanNumberError("No se puede restar C a un valor distinto de D o M")
+            if caracter_anterior == "V" or caracter_anterior == "L" or caracter_anterior == "D":
+                raise RomanNumberError("No se puede restar V o L o D")
+            if caracter_anterior == compro and (caracter_anterior == "I" or caracter_anterior == "X" or caracter_anterior == "C"):
+                raise RomanNumberError("No se puede restar mas de dos veces I o C o X")
             
+
+               
+            r -= simbolo_romano[caracter_anterior] * 2
+        compro = caracter_anterior
 
         r += simbolo_romano[caracter]
         caracter_anterior = caracter
         
-    
+    #print(r)
     return r
+
+romano_a_entero2("C")
--- test_entero_a_romano.py
+++ test_entero_a_romano.py
@@ -1,3 +1,4 @@
+from turtle import st
 import pytest
 
 
@@ -36,3 +37,29 @@ def test_romano_a_entero_no_mas_de_tres():
 
 def test_romano_a_entero_resta_si_soy_mayor_anterior():
     assert romano_a_entero2("IV") == 4
+
+def test_romano_a_entero_d_l_v_mas_de_dos_veces():
+    with pytest.raises(RomanNumberError) as exceptionInfo:
+        romano_a_entero2("LVV")
+    assert str(exceptionInfo.value) == "No se puede repetir D o L o V mas de una vez"
+
+def test_romano_a_entero_restar_X_a_V_C():
+    with pytest.raises(RomanNumberError) as exceptionInfo:
+            romano_a_entero2("XD")
+    assert str(exceptionInfo.value) == "No se puede restar X a un valor distinto de L o C"
+
+def test_romano_a_entero_restar_I_a_V_X():
+    with pytest.raises(RomanNumberError) as exceptioninfo:
+        romano_a_entero2("MIL")
+    assert str(exceptioninfo.value) == "No se puede restar I a un valor distinto de X o V"
+
+def test_romano_a_entero_restar_V_L_D():
+    with pytest.raises(RomanNumberError) as exceptionInfo:
+            romano_a_entero2("VX")
+    assert str(exceptionInfo.value) == "No se puede restar V o L o D"
+
+def test_romano_a_entero_restar_I_X_C():
+    with pytest.raises(RomanNumberError) as exceptionInfo:
+        romano_a_entero2("IIV")
+    assert str(exceptionInfo.value) == "No se puede restar mas de dos veces I o C o X"
+
